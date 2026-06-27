a440b774cac16ee02554ed159cc441c110308bbf
GotchaProduction4 <goldilocks5672@gmail.com>
2026-06-28 00:59:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ document.addEventListener("click", function(e) {
     sound.volume = 0.5;
     sound.play();
 });
+
+document.addEventListener("click", function(e) {
+    let sound = document.getElementById("clickSound");
+    if (sound) {
+        sound.currentTime = 0;
+        sound.play();
+    }
+});
