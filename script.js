9a2615595166978b0a39c689fedd675b0d80b359
GotchaProduction4 <goldilocks5672@gmail.com>
2026-06-27 22:33:31 +0800

    Add click event listener to play sound on button click

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+document.addEventListener("click", function(e) {
+    if (e.target.tagName === "BUTTON") {
+        let sound = document.getElementById("clickSound");
+        sound.currentTime = 0;
+        sound.play();
+    }
+});
