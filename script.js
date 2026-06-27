319ef3b417854cade65d5c9990fbc88c8554413e
GotchaProduction4 <goldilocks5672@gmail.com>
2026-06-27 22:49:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 document.addEventListener("click", function(e) {
-    if (e.target.tagName === "BUTTON") {
-        const sound = document.getElementById("clickSound");
+    const sound = document.getElementById("clickSound");
 
-        if (sound) {
-            sound.currentTime = 0;
-            sound.play();
-        }
+    if (!sound) return;
+
+    // play sound for ALL clickable elements
+    if (
+        e.target.tagName === "BUTTON" ||
+        e.target.tagName === "A"
+    ) {
+        sound.currentTime = 0;
+        sound.play();
     }
 });
