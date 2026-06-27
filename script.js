c7bcc7e71d5999c9d549239b576fa7ce241cd40c
GotchaProduction4 <goldilocks5672@gmail.com>
2026-06-27 22:44:15 +0800

    Change let to const for sound variable

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("click", function(e) {
     if (e.target.tagName === "BUTTON") {
-        let sound = document.getElementById("clickSound");
+        const sound = document.getElementById("clickSound");
+
         if (sound) {
             sound.currentTime = 0;
             sound.play();
