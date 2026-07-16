61be49ff3e9b0bd8527d1c581adb44c2e11559c8
GotchaProduction4 <goldilocks5672@gmail.com>
2026-07-16 11:57:58 +0800

    Implement click sound effect
    Added audio feedback on click events.

--- script.js
+++ script.js
@@ -1,32 +1,5 @@
-const modal=document.getElementById("contactModal");
-
-const btn=document.getElementById("contactBtn");
-
-const close=document.querySelector(".close");
-
-btn.onclick=function(e){
-
-e.preventDefault();
-
-modal.style.display="flex";
-
-}
-
-close.onclick=function(){
-
-modal.style.display="none";
-
-}
-
-window.onclick=function(e){
-
-if(e.target===modal){
-
-modal.style.display="none";
-
-}
-
-}document.addEventListener("click", function(e) {
+document.addEventListener("click", function(e) {
+    
     const sound = new Audio("sounds/click1.mp3");
     sound.volume = 0.5;
     sound.play();
