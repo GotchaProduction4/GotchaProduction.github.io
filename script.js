d17d3240d28b31e5bfde8550f840269be445781f
GotchaProduction4 <goldilocks5672@gmail.com>
2026-07-16 11:49:49 +0800

    Implement modal functionality and sound effect

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-document.addEventListener("click", function(e) {
+const modal=document.getElementById("contactModal");
+
+const btn=document.getElementById("contactBtn");
+
+const close=document.querySelector(".close");
+
+btn.onclick=function(e){
+
+e.preventDefault();
+
+modal.style.display="flex";
+
+}
+
+close.onclick=function(){
+
+modal.style.display="none";
+
+}
+
+window.onclick=function(e){
+
+if(e.target===modal){
+
+modal.style.display="none";
+
+}
+
+}document.addEventListener("click", function(e) {
     const sound = new Audio("sounds/click1.mp3");
     sound.volume = 0.5;
     sound.play();
